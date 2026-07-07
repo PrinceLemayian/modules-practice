5863cb6032cd5ff52d58dcda8aacc430ace2426f
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-07-07 23:52:45 +0300

    refactor: apply clean code principles to budget app

---
+++ clean.js
@@ -0,0 +1,48 @@
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freentryancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => (spendingLimits[user] ? spendingLimits[user] : 0);
+
+const addExpense = function (value, description, user = 'jonas') {
+  if (!user) user = 'jonas';
+  user = user.toLowerCase();
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpenses = function () {
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+};
+checkExpenses();
+
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
+logBigExpenses(1000);
